5139d05a5945fe8ed1698c9d5a13a45bbc57d50a
Marcus Nitzschke <marcus.nitzschke@gmx.com>
2013-03-21 01:45:43 +0100

    added ELPA conform header including license

--- sparql-mode.el
+++ sparql-mode.el
@@ -1,8 +1,37 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; Interactively evaluate SPARQL
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; sparql-mode.el --- Interactively evaluate SPARQL
+
+;; Copyright (C) 2011  Craig Andera
+;; Copyright (C) 2013  Marcus Nitzschke
+
+;; Author: Craig Andera <candera@wangdera.com>
+;; Version: 0.0.1
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Usage:
+
+;; Add to your emacs config:
+
+;;  (add-to-list 'load-path "/path/to/sparql-mode-dir")
+;;  (autoload 'sparql-mode "sparql-mode.el"
+;;   "Major mode for editing SPARQL files" t)
+;;  (add-to-list 'auto-mode-alist '("\\.sparql$" . sparql-mode))
+
 
 (defgroup sparql nil
   "Major mode for editing and evaluating SPARQL queries."
@@ -131,3 +160,5 @@ If the region is not active, use the whole buffer."
   (define-key sparql-mode-map (kbd "C-c C-x") 'sparql-query-region))
 
 (provide 'sparql-mode)
+
+;;; sparql-mode.el ends here
