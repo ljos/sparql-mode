99df42bf76a9881d87219cbd39d5bf690c00e6a6
Bjarte Johansen <bjarte.johansen@gmail.com>
2013-05-06 12:46:29 +0200

    update version number

--- sparql-mode.el
+++ sparql-mode.el
@@ -7,7 +7,7 @@
 ;; Author: Craig Andera <candera at wangdera dot com>
 ;; Maintainer: Bjarte Johansen <Bjarte dot Johansen at gmail dot com>
 ;; Homepage: https://github.com/ljos/sparql-mode
-;; Version: 0.4.0
+;; Version: 0.5.0
 
 ;; This file is not part of GNU Emacs.
 
