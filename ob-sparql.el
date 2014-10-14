e21a5a035f077521900f93ec0de864af2b75db8f
Jacek Grzebyta <jgrzebyta@gmail.com>
2014-10-14 16:38:09 +0100

    Closes #27: Fix doubled 'Accept:' header
    If the `Accept` header parameter is added into the
    `url-request-extra-headers` variable then the url package passed to the
    server becomes:
    
    ```
    Accept: */*
    User-Agent: URL/Emacs
    Content-Type: application/x-www-form-urlencoded
    Accept: xxxxxxxx ;; user variable
    ```
    
    In that case some servers ignore the second `Accept` and just use the
    default value (i.e. sparql/xml). The accepted mime type should be
    configured by entering the proper value into the
    `url-mime-accept-string` variable.

--- ob-sparql.el
+++ ob-sparql.el
@@ -56,10 +56,10 @@ to do that."
   (message "Executing a SPARQL query block.")
   (let ((endpoint-url (cdr (assoc :url params)))
         (url-request-method "POST")
+        (url-mime-accept-string (cdr (assoc :format params)))
         (url-request-data (format "query=%s" (url-hexify-string body)))
         (url-request-extra-headers
-         `(("Content-Type" . "application/x-www-form-urlencoded")
-           ("Accept" . ,(cdr (assoc :format params))))))
+         `(("Content-Type" . "application/x-www-form-urlencoded"))))
     (with-current-buffer (url-retrieve-synchronously endpoint-url)
       (if (zerop (buffer-size))
           (error "error: URL '%s' is not accessible." endpoint-url)
