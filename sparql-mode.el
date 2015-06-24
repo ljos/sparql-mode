a7895d197500c028ce72859e90f0576ca07bbb88
Don Pellegrino <dapellegrino@dow.com>
2015-06-24 11:07:54 -0400

    W3C SPARQL Query Language for RDF 15 January 2008 recommends that sparql query files have the extension .rq.

--- sparql-mode.el
+++ sparql-mode.el
@@ -40,6 +40,7 @@
 ;;  (autoload 'sparql-mode "sparql-mode.el"
 ;;   "Major mode for editing SPARQL files" t)
 ;;  (add-to-list 'auto-mode-alist '("\\.sparql$" . sparql-mode))
+;;  (add-to-list 'auto-mode-alist '("\\.rq$" . sparql-mode))
 
 ;;; Code:
 (require 'cl-lib)
